778e810e2b01aa470c677a05026b556ef66e727a
kiranuma890 <kiranuma890@gmail.com>
2025-06-23 14:46:37 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,11 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: ["https://gen-z-frontend.onrender.com"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 mongoose
   .connect(process.env.MONGO_URI, {
